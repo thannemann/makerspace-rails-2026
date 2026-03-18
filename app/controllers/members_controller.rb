9aacb9997f16497c480fc673bf5cade50204779a
Kevin Kadow <99362831+Kv603@users.noreply.github.com>
2026-03-17 21:06:05 -0400

    Refactor member index query for admin checks

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -4,23 +4,29 @@ class MembersController < AuthenticationController
 
     def index
       base_query = Member.includes(:access_cards).includes(:earned_membership)
-      raise ::Error::Forbidden.new unless is_admin?
-      # Limit index to only current members unless authorized and requesting full records
-      if !is_admin? || to_bool(search_params[:current_members])
-        # Include unset or expired within grace period
-        search = base_query.where({
-          :$or => [
-            { :expirationTime.gte => ((Time.now + 3.days).strftime('%s').to_i * 1000) },
-            {  expirationTime: nil }
-          ]
-        })
+      if is_admin?
+        # Limit index to only current members unless authorized and requesting full records
+        if to_bool(search_params[:current_members])
+          # Include unset or expired within grace period
+          search = base_query.where({
+            :$or => [
+              { :expirationTime.gte => ((Time.now + 3.days).strftime('%s').to_i * 1000) },
+              {  expirationTime: nil }
+            ]
+          })
+        else
+          search = Mongoid::Criteria.new(base_query)
+        end
       else
-        search = Mongoid::Criteria.new(base_query)
+        # Non-admin users can only see their own record
+        if !current_member.earned_membership
+          search = base_query.where(id: current_member.id)
       end
       @members = query_resource(search)
 
       return render_with_total_items(@members, { each_serializer: MemberSummarySerializer, adapter: :attributes })
     end
+
 
     def show
       render json: @member, adapter: :attributes and return
