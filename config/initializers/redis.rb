929b6cdc240629b4eee5f0438820debab80600a5
Kevin Kadow <99362831+Kv603@users.noreply.github.com>
2026-03-26 10:18:43 -0400

    Remove db parameter from Redis initialization

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,4 +1,5 @@
+#db: ENV['REDIS_DB'],
 Redis.current = Redis.new(url: ENV['REDIS_URL'],
                           port: ENV['REDIS_PORT'],
-                          db: ENV['REDIS_DB'],
+                          
                           ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE })
