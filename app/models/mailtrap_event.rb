476702a0169896b995627cddde04f9f3d54c5611
Kevin Kadow <99362831+Kv603@users.noreply.github.com>
2026-04-24 17:06:32 -0400

    Add new fields to MailtrapEvent model

--- app/models/mailtrap_event.rb
+++ app/models/mailtrap_event.rb
@@ -9,4 +9,12 @@ class MailtrapEvent
   field :email, type: String
   field :status, type: String
   field :occurred_at, type: Time
+  field :event, type: String
+  field :event_id, type: String
+  field :message_id, type: String
+  field :response, type: String
+  field :sending_stream, type: String
+  field :sending_domain_name, type: String
+  field :timestamp, type: Integer
+  field :raw_payload, type: Hash
 end
