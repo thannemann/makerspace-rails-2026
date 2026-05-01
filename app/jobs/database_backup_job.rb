d8fbed1e7b7cd1800f06da30d37abd4d5e870aca
thannemann <thannemann@gmail.com>
2026-04-30 22:34:58 -0400

    Update database_backup_job.rb
    correcting backup call 2 places

--- app/jobs/database_backup_job.rb
+++ app/jobs/database_backup_job.rb
@@ -1,11 +1,10 @@
 class DatabaseBackupJob < ApplicationJob
   queue_as :default
-
   def perform
     Rails.application.load_tasks
     begin
-      Rake::Task["db:backup"].reenable
-      Rake::Task["db:backup"].invoke
+      Rake::Task["backup"].reenable
+      Rake::Task["backup"].invoke
       SystemConfig.record_run("db_backup", success: true)
     rescue => e
       SystemConfig.record_run("db_backup", success: false)
