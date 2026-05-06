5b9a129694e2b6093202fa62445173c254cfe2d9
thannemann <thannemann@gmail.com>
2026-05-05 22:08:20 -0400

    Update upload.rb
    clean up the google back trace error to a cleaner message

--- lib/error/google/upload.rb
+++ lib/error/google/upload.rb
@@ -2,7 +2,21 @@ require_relative '../custom_error'
 module Error::Google
   class Upload < ::Error::CustomError
     def initialize(err=nil)
-      super(:internal_server_error, 500, err || 'Error uploading file to Google')
+      message = if err.nil?
+        'Error uploading file to Google'
+      elsif err.respond_to?(:body)
+        body = JSON.parse(err.body) rescue nil
+        if body && body['error_description'].present?
+          "Google Drive upload failed: #{body['error_description']}"
+        elsif body && body['error'].present?
+          "Google Drive upload failed: #{body['error']}"
+        else
+          "Google Drive upload failed: #{err.message}"
+        end
+      else
+        "Google Drive upload failed: #{err}"
+      end
+      super(:internal_server_error, 500, message)
     end
   end
 end
